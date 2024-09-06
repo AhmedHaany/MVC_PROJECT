d355c6a6bf4414189fdc1537d1dc0843020526ca
AhmedHaany <shmed2001@gmail.com>
2024-09-06 20:56:54 +0300

    test

---
+++ DataAccessLayer/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Demo.DataAccessLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DataAccessLayer.Models
+{
+    public class DataContext :DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server = . ; trusted_connection = true ; trust server certificate = true ; database = ");
+        //}
+
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Demo.PresentationLayer/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Demo.BusinessLogicLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.PresentationLayer.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly IDepartmentRepository DepartmentRepository;
+
+        public DepartmentsController(IDepartmentRepository departmentRepository)
+        {
+            DepartmentRepository = departmentRepository;
+        }
+
+        public IActionResult Index()
+        {
+
+            
+            //Retrieve All Departments 
+            return View();
+        }
+    }
+}
